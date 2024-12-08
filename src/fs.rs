3546d4c2908064ec1db367de6921aff23bb33dec
Ada Bohm <ada@kreatrix.org>
2024-12-08 17:32:50 +0100

    Added xtask-cli to simplify xtasks that uses kompari

--- src/fs.rs
+++ src/fs.rs
@@ -1,23 +1,33 @@
 // Copyright 2024 the Kompari Authors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-use std::ffi::OsString;
-use std::path::Path;
+use std::ffi::{OsStr, OsString};
+use std::path::{Path, PathBuf};
 
-pub fn read_images_from_dir(path: &Path) -> crate::Result<Vec<OsString>> {
-    Ok(std::fs::read_dir(path)?
-        .flatten()
-        .filter_map(|path| {
+pub(crate) fn list_image_dir(
+    dir_path: &Path,
+) -> Result<impl Iterator<Item = PathBuf>, std::io::Error> {
+    Ok(std::fs::read_dir(dir_path)?.filter_map(|entry| {
+        if let Ok(entry) = entry {
+            let path = entry.path();
             if path
-                .path()
                 .extension()
-                .and_then(|p| p.to_str())
-                .map_or(false, |p| p.to_ascii_lowercase() == "png")
+                .and_then(OsStr::to_str)
+                .map(|ext| ext.to_ascii_lowercase() == "png")
+                .unwrap_or(false)
             {
-                Some(path.file_name())
+                Some(path)
             } else {
                 None
             }
-        })
-        .collect())
+        } else {
+            None
+        }
+    }))
+}
+
+pub(crate) fn list_image_dir_names(
+    dir_path: &Path,
+) -> Result<impl Iterator<Item = OsString>, std::io::Error> {
+    Ok(list_image_dir(dir_path)?.filter_map(|p| p.file_name().map(|name| name.to_os_string())))
 }
